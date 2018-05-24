173abe4b3d0b8fe2b78691e3519814c419314c61
kermitnirmit <kermitnirmit@gmail.com>
2018-05-23 23:12:18 -0500

    fine tuning the rate-limiter time

--- Pauser.js
+++ Pauser.js
@@ -6,7 +6,7 @@ class Pauser {
         for (const x in this.tracked) {
           if (this.tracked[x] > 0) this.tracked[x]--
         }
-      }, 5000)
+      }, 7000)
     }
   
     check (message) {
